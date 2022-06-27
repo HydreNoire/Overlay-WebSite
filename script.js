45faaa5d7091592bb15d53bb98f9452de096d940
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-06-27 10:57:43 +0200

    Add JS and CSS on all article

--- script.js
+++ script.js
@@ -7,27 +7,60 @@
 //       overlayDiv.style.opacity = 0;
 //   }
 
+// function showArticle(article) {
+//   if (article.style.display === "none") {
+//     article.style.display = "block";
+//     header.style.display = "none";
+//     blur.style.display = "block";
+//   } else {
+//     article.style.display = "none";
+//     blur.style.display = "none";
+//   }
+// }
+
 let blur = document.getElementById("blur");
-let btnIntro = document.getElementById("intro-btn");
-let introArticle = document.getElementById("intro-article");
-let close = document.getElementById("close");
 let header = document.getElementById("header");
+// Boutons d'action
+let btnIntro = document.getElementById("intro-btn");
+let btnAbout = document.getElementById("about-btn");
+let btnWork = document.getElementById("work-btn");
+let btnContact = document.getElementById("contact-btn");
+// Articles
+let introArticle = document.getElementById("intro-article");
+let aboutArticle = document.getElementById("about-article");
+let workArticle = document.getElementById("work-article");
+let contactArticle = document.getElementById("contact-article");
 
-btnIntro.addEventListener("click", showArticle);
+btnIntro.addEventListener("click", () => {
+  introArticle.style.display = "flex";
+  header.style.display = "none";
+  blur.style.display = "flex";
+});
+btnAbout.addEventListener("click", () => {
+  aboutArticle.style.display = "flex";
+  header.style.display = "none";
+  blur.style.display = "flex";
+});
+btnWork.addEventListener("click", () => {
+  workArticle.style.display = "flex";
+  header.style.display = "none";
+  blur.style.display = "flex";
+});
+btnContact.addEventListener("click", () => {
+  contactArticle.style.display = "flex";
+  header.style.display = "none";
+  blur.style.display = "flex";
+});
 
-function showArticle() {
-  if (introArticle.style.display === "none") {
-    introArticle.style.display = "block";
-    header.style.display = "none";
-    blur.style.display = "block";
-  } else {
+let close = document.querySelectorAll(".quit");
+
+close.forEach(item => {
+  item.addEventListener('click', (event) => {
     introArticle.style.display = "none";
+    aboutArticle.style.display = "none";
+    workArticle.style.display = "none";
+    contactArticle.style.display = "none";
     blur.style.display = "none";
-  }
-}
-
-close.addEventListener("click", function() {
-  introArticle.style.display = "none";
-  blur.style.display = "none";
-  header.style.display = "flex";
+    header.style.display = "flex";
+  })
 })
